b52c14a920be26eb2d41e8772650d0cde467d94b
Abin John <109900435+Abinjohn1245@users.noreply.github.com>
2025-08-05 22:52:42 +0530

    Add files via upload

---
+++ Project6(Simple Grading System).py
@@ -0,0 +1,21 @@
+a=int(input('Enter Your Marks:'))
+
+if a>=90:
+    print('A+')
+elif a>=80:
+    print('A')
+elif a>=70:
+    print('B+')
+elif a>=60:
+    print('B')
+elif a>=50:
+    print('C+')
+elif a>=40:
+    print('C')
+elif a>=30:
+    print('D+')
+elif a>=20:
+    print('D')
+else:
+    print('Not Passed')
+    
